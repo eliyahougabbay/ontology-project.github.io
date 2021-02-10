c82691154176749999a0d9f373a7c182955db06b
= <sebastien.stempak@gmail.com>
2021-02-10 23:19:24 +0100

    added path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from owlready2 import *
-onto_path.append(r"C:\Users\Eliyahou\IMTMinesAles\Ontologie\ontology-project.github.io")
+# onto_path.append(r"C:\Users\Eliyahou\IMTMinesAles\Ontologie\ontology-project.github.io")
+onto_path.append(r"C:\Users\Miste\Documents\2IA\10.2_Web_sem\ontology-project.github.io")
 onto = get_ontology("SportsX.owl")
 onto.load()
